4fc8491ea359bbc9008febf50424b7f9d31f353d
SiddharthChoudhary <firesid12@gmail.com>
2018-05-31 17:33:59 +0530

    Update 2018_05_30_155005_create_profiles_table.php
    Alterations in migration

--- database/migrations/2018_05_30_155005_create_profiles_table.php
+++ database/migrations/2018_05_30_155005_create_profiles_table.php
@@ -19,8 +19,8 @@ class CreateProfilesTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->string('username');
             $table->biginteger('uid')->unsigned();
-            $table->string('access_token');
-            $table->string('access_token_secret');
+            $table->string('access_token')->nullable();
+            $table->string('access_token_secret')->nullable();
             $table->timestamps();
         });
     }
